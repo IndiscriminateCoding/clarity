45fc68b3df8530ccd7e52b9d2d92785902fbf1ff
IndiscriminateCoding <joyprophecy@gmail.com>
2017-05-08 15:54:33 -0400

    fix Reader.zip type

--- lib/reader.mli
+++ lib/reader.mli
@@ -7,5 +7,5 @@ external reader : ('r -> 'a) -> ('r, 'a) t = "%identity"
 val ask : ('r, 'r) t
 val local : ('r -> 'r) -> ('r, 'a) t -> ('r, 'a) t
 val dimap : ('q -> 'r) -> ('a -> 'b) -> ('r, 'a) t -> ('q, 'b) t
-val zip : ('r, 'b) t -> ('r, 'b) t -> ('r, 'b * 'b) t
+val zip : ('r, 'a) t -> ('r, 'b) t -> ('r, 'a * 'b) t
 
